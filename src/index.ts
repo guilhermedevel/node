9e3b322e03922028b66680dbc2a85a37978e2ba9
guilhermedevel <guilherme.zarzur@gmail.com>
2024-04-05 14:58:54 -0300

    Installing and setting ExpressJS

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import validator from "validator";
-
-let ip: string = '192.0.0.256';
-let email: string = 'gzarzur@dev.com';
-
-console.log(`Verificando se o email ${email} é valido: ${validator.isEmail(email)}`);
-console.log(`Verificando se o ip ${ip} é válido: ${validator.isIP(ip)}`);
