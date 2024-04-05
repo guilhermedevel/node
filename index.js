2402dfb86d1a2ea311d751d4f35819241dd7dfe2
guilhermedevel <guilherme.zarzur@gmail.com>
2024-04-05 00:18:48 -0300

    Basic TS configuration

--- index.js
+++
@@ -1,3 +0,0 @@
-let name = 'gzarzur';
-let age = 34;
-console.log(`My name is ${name} and i'm ${age} years old!`);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let nome: string = 'gzarzur';
+let age: number = 34;
+
+console.log(`My name is ${nome} and i'm ${age} years old!`);
