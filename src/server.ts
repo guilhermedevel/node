d148ac58cdbd09c9f1229e76f6a90c6300ca4c03
guilhermedevel <guilherme.zarzur@gmail.com>
2024-04-16 10:05:54 -0300

    Dynamic routes test

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,19 @@ import express, {Request, Response} from 'express';
 const server = express();
 
 server.get('/', (req: Request, res: Response) =>{
-    res.send('Hello World!')
+    res.send('Hello World!');
+});
+
+server.get('/noticia/:slug', (req: Request, res: Response) =>{
+    let slug = req.params.slug;
+
+    res.send(`Noticia: ${slug}`);
+});
+
+server.get('/voo/:origem-:destino', (req: Request, res: Response) =>{
+    let {origem, destino} = req.params;
+
+    res.send(`Exibindo voo de ${origem.toUpperCase()} para ${destino.toUpperCase()}`);
 });
 
 server.listen(80);
