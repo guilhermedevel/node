60d5f2d6d9937ab3ab8573ee892f57285efbdac8
guilhermedevel <guilherme.zarzur@gmail.com>
2024-04-16 11:32:47 -0300

    Working with the Router

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
+import mainRouters from './routes/index';
+import painelRouters from './routes/painel';
 
 const server = express();
 
-server.get('/', (req: Request, res: Response) =>{
-    res.send('Hello World!');
-});
-
-server.get('/noticia/:slug', (req: Request, res: Response) =>{
-    let slug = req.params.slug;
-
-    res.send(`Noticia: ${slug}`);
-});
-
-server.get('/voo/:origem-:destino', (req: Request, res: Response) =>{
-    let {origem, destino} = req.params;
-
-    res.send(`Exibindo voo de ${origem.toUpperCase()} para ${destino.toUpperCase()}`);
-});
+server.use('/', mainRouters);
+server.use('/painel', painelRouters);
 
 server.listen(80);
