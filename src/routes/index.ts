a811d47e5852e91acb97f2c5238c988117d4f91c
guilhermedevel <guilherme.zarzur@gmail.com>
2024-05-04 11:11:59 -0300

    Mustache: Partial content

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,7 +12,7 @@ router.get('/', (req: Request, res: Response) =>{
         showOld = true;
     };
 
-    res.render('home', {
+    res.render('pages/home', {
         name,
         lastName: 'Zarzur',
         age,
@@ -27,11 +27,11 @@ router.get('/', (req: Request, res: Response) =>{
 });
 
 router.get('/contato', (req: Request, res: Response) => {
-    res.send('Página de contato')
+    res.render('pages/contato')
 });
 
 router.get('/sobre', (req: Request, res: Response) => {
-    res.send('Página institucional sobre a empresa')
+    res.render('pages/sobre')
 });
 
 export default router;
